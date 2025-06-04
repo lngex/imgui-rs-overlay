b8b12a1dc356b6cbf7b45c7543cad6b2dcad1a17
lingex <1902156923@qq.com>
2025-06-04 17:46:14 +0800

    Remove excess imgui dependencies

--- examples/hello.rs
+++ examples/hello.rs
@@ -1,25 +1,27 @@
 use std::borrow::Cow;
 
 use imgui::Condition;
+use windows::Win32::UI::WindowsAndMessaging::GetDesktopWindow;
 
 use imgui_rs_overlay::{key_down, OverlayTarget};
 use imgui_rs_overlay::window::{FrameRate, Windows, WindowsOptions};
 
 fn main() -> imgui_rs_overlay::Result<()> {
     let mut index = 2usize;
-    let items = ["深色", "高亮", "经典"];
+    let items = ["Dark", "Highlight", "Classic"];
     let mut app = Windows::new(&WindowsOptions {
         frame_rate: FrameRate::UN_LIMITED,
-        overlay_target:OverlayTarget::WindowTitle(String::from("计算器")),
+        overlay_target: OverlayTarget::Window(unsafe { GetDesktopWindow() }),
+        style_init:Some(Box::new(|imgui|{})),
         ..WindowsOptions::default()
     })?;
     app.run(move |ui, style| {
         ui.window("imgui")
             .resizable(false)
-            .size([150.0, 100.0], Condition::FirstUseEver)
+            .size([250.0, 100.0], Condition::FirstUseEver)
             .movable(true)
             .build(|| {
-                if ui.combo("主题", &mut index, &items, |item| {
+                if ui.combo("theme", &mut index, &items, |item| {
                     Cow::Owned(String::from(*item))
                 }) {
                     match index {
@@ -30,9 +32,10 @@ fn main() -> imgui_rs_overlay::Result<()> {
                     };
                 }
                 ui.text(format!("FPS: {:.2}", ui.io().framerate));
-                ui.text("你好世界!");
+                ui.text("hello world!");
             });
         !key_down!(35)
     })?;
     Ok(())
 }
+
